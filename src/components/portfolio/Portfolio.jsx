2a437b4108dc7b84c191f06393b45c3fbaa283bf
Mayukh <mayukh1995@gmail.com>
2024-01-02 20:58:17 +0530

    links added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,24 +8,28 @@ const items = [
     title: "Blogger",
     img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     desc: "A blog application integrated with Next-auth authetication, mongodb backend and comment features",
+    link:"",
   },
   {
     id: 2,
     title: "Sushiman",
     img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     desc: "A UI/UX design project for a Sushi Restaurant",
+    link:"https://6592dd831f7c2bd38cf0dfed--scintillating-speculoos-80af6c.netlify.app/"
   },
   {
     id: 3,
     title: "Hilink",
     img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     desc: "A UI/UX design project for a Travel company",
+    link:"https://travel-steel-three.vercel.app/"
   },
   {
     id: 4,
     title: "Threads Clone",
     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     desc: "A Threads Clone with mongodb backend and Clerk authentication integration",
+    link:"https://thread-pi.vercel.app/"
   },
 
   {
@@ -33,6 +37,7 @@ const items = [
     title: "Snapgram",
     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     desc: "An Instagram clone powered by AppWrite",
+    link:""
   },
 ];
 
@@ -55,7 +60,11 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{y}}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
+            <button onClick={()=>window.open(`${item.link}`)} >
+              See Demo    
+             
+              
+              </button>
           </motion.div>
         </div>
       </div>
