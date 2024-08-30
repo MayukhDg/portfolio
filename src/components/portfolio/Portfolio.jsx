566fbc392befdcfc369e0a1e3d3dfd385c5dbece
Mayukh <mayukh1995@gmail.com>
2024-08-30 22:50:08 +0530

    ...

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -29,7 +29,7 @@ const items = [
     title: "Threads Clone",
     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     desc: "A Threads Clone with mongodb backend and Clerk authentication integration",
-    link:"https://thread-pi.vercel.app/"
+    link:"https://thread-livid.vercel.app/"
   },
 
 ];
