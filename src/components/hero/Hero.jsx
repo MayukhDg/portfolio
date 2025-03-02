06feea253ea24a6e18911fddaccab0bdbe708364
Your Name <mayukh1995@gmail.com>
2025-03-02 10:26:31 +0530

    ...

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -71,7 +71,7 @@ const Hero = () => {
         initial="initial"
         animate="animate"
       >
-        Writer Content Creator Influencer
+        Full Stack Web Developer 
       </motion.div>
       <div className="imageContainer">
         <img src="/hero.png" alt="" />
