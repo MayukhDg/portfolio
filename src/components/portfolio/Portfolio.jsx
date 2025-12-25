027770e0909904ff8a2ccab1ec820ab8d72906a1
Your Name <mayukh1995@gmail.com>
2025-12-25 23:33:50 +0530

    ...

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -26,6 +26,14 @@ const items = [
     desc: "Transform images seamlessly with AI. Enhance, upscale, and modify images effortlessly using cutting-edge technology.",
     link:"https://imaginify-nine-sable.vercel.app/",
   },
+
+  {
+    id: 4,
+    title: "BidWinnerAI",
+    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Write winning proposals in minutes with AI. BidWinnerAI crafts tailored proposals that increase your chances of winning projects.",
+    link:"https://www.bidwinner.shop/",
+  },
 ];
 
 const Single = ({ item }) => {
